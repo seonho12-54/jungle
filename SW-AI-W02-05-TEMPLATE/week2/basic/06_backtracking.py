85344fe1e2321f67697ec3159d1023612a3d4d4e
seonho <hikkcom40@gmail.com>
2026-03-09 17:40:16 +0900

    6번문제풀이

--- SW-AI-W02-05-TEMPLATE/week2/basic/06_backtracking.py
+++ SW-AI-W02-05-TEMPLATE/week2/basic/06_backtracking.py
@@ -23,6 +23,8 @@
 """
 
 def combinations(n, k):
+    
+    
     """
     1부터 n까지 숫자 중 k개를 선택하는 모든 조합 찾기
     
@@ -35,24 +37,17 @@ def combinations(n, k):
     """
     result = []
     
-    def backtrack(start, current_combination):
-        """
-        백트랙킹 헬퍼 함수
-        
-        Args:
-            start: 탐색을 시작할 숫자
-            current_combination: 현재까지 선택한 숫자들
-        """
-        # TODO: base case - k개를 모두 선택했으면 결과에 추가
-        pass
-        
-        # TODO: start부터 n까지 숫자를 하나씩 시도
-        ## TODO: 백트랙킹 3단계 구현
-        ## 1. 선택(Choose)
-        ## 2. 탐색(Explore)
-        ## 3. 취소(Unchoose)
-        pass
-    
+
+    def backtrack(start, current):
+        if len(current) == k:
+            result.append(current[:])
+            return
+
+        for i in range(start, n + 1):
+            current.append(i)
+            backtrack(i + 1, current)
+            current.pop()
+
     backtrack(1, [])
     return result
 
