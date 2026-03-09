93904a9d846d63a2590c305522ee9b411535f1ba
seonho <hikkcom40@gmail.com>
2026-03-09 14:18:48 +0900

    4번문제풀이

--- problem-solving/단어공부.py
+++ problem-solving/단어공부.py
@@ -1,2 +1,27 @@
 
-for i in range(strr):
+
+name = input()
+name = name.lower()
+
+
+strr = [0]*26
+
+num=0
+for i in name:
+    num = ord(i) - ord('a')
+    strr[num] +=1
+    
+    
+    
+count = 0
+
+for i in range(len(strr)):
+      if strr[i] == max(strr):
+            count +=1
+            
+        
+        
+        
+if count >= 2:
+    print("?")
+else:    print(chr(strr.index(max(strr))+ord('a')).upper())
