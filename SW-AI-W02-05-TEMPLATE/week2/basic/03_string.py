7b4917437fcf9d4242baebfddc40c90ac3995812
seonho <hikkcom40@gmail.com>
2026-03-09 11:38:20 +0900

    3번문제품

--- SW-AI-W02-05-TEMPLATE/week2/basic/03_string.py
+++ SW-AI-W02-05-TEMPLATE/week2/basic/03_string.py
@@ -25,6 +25,30 @@
 """
 
 def is_palindrome(s):
+    
+    s = s.lower()
+    ns = ""
+    for i in s:
+        if i.isalnum():
+            ns += i
+    #ns은 특수기호 없애고 소문자로 바꾼것
+    
+    #추가로 만들것은 반대로 된것을 확인하고 비교하는것
+    rv =""
+    rv = rv.join(reversed(ns))
+    
+    count = 0
+    for i in range(len(ns)):
+        if ns[i] == rv[i]:
+            count += 1
+    
+    if count == len(ns):
+        return True
+    else:
+        return False
+    
+    
+    
     """
     문자열이 회문인지 판별하는 함수
     
