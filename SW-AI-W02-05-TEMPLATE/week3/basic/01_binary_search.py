36d3e004b57547f9b3231fc49d98b97916fc67f0
seonho <hikkcom40@gmail.com>
2026-03-13 15:59:17 +0900

    변경사항

--- SW-AI-W02-05-TEMPLATE/week3/basic/01_binary_search.py
+++ SW-AI-W02-05-TEMPLATE/week3/basic/01_binary_search.py
@@ -33,8 +33,30 @@ def binary_search(arr, target):
     Returns:
         target의 인덱스 (없으면 -1)
     """
+    #타겟이 없으면 -1 리턴이고 있으면 break 돼서 끝남
+    #중간인덱스 mid 값계산
+    #타겟이 중간인덱스보다 크면 왼쪽, 작으면 오른쪽으로 범위 조정
+    
+    
+    
+    
+    
+    
     left = 0
     right = len(arr) - 1
+    
+    
+    
+    while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+    return -1
+    
     
     # TODO: left가 right보다 작거나 같을 때까지 반복
     ## 중간 인덱스 계산
@@ -44,7 +66,11 @@ def binary_search(arr, target):
     ## target이 더 작으면 right = mid - 1
     pass
     
-    return -1
+    
+    
+    
+    
+    
 
 # 테스트 케이스
 if __name__ == "__main__":
